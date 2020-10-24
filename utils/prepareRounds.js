9d2054c0a6b9bd9fbf1ed3aaa41c466339a9fb78
Doaa Mahely <dmahely@hotmail.com>
2020-10-25 00:42:01 +0400

    Add missing function

--- utils/prepareRounds.js
+++ utils/prepareRounds.js
@@ -4,15 +4,16 @@ const fetchAlbums = require('../api/fetchAlbums');
 const fetchArtistDetails = require('../api/fetchArtistDetails');
 const fetchRelatedArtists = require('../api/fetchRelatedArtists');
 const extractAlbumData = require('./extractAlbumData');
+const extractArtistData = require('./extractArtistsData');
 const extractRelatedArtists = require('./extractRelatedArtists');
 const getCurrentArtistId = require('./getCurrentArtistId');
 
 const prepareRounds = async (accessToken, currentRound = 1) => {
     const randomAlbums = await fetchAlbums(accessToken);
-
     const albums = extractAlbumData(randomAlbums);
 
-    const artistImages = await fetchArtistDetails(accessToken, albums);
+    const artistDetails = await fetchArtistDetails(accessToken, albums);
+    const artistImages = extractArtistData(artistDetails);
 
     // map each round's artists obj to an artist image
     const rounds = albums.map((album, index) => {
@@ -23,7 +24,6 @@ const prepareRounds = async (accessToken, currentRound = 1) => {
     const artistId = getCurrentArtistId(albums, currentRound);
 
     const allRelatedArtists = await fetchRelatedArtists(accessToken, artistId);
-
     const relatedArtists = extractRelatedArtists(allRelatedArtists);
 
     return { rounds, relatedArtists, currentRound };
